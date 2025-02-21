5345e5c41e79017c159574af91ae10af4b6d5bb7
PagaMin <financeiroarthur09@gmail.com>
2025-02-20 22:15:42 -0300

    first-commit

---
+++ src/test/java/com/example/auth_learning/AuthLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auth_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
