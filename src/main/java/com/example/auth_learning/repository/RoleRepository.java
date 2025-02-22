d7c68d6c7d7b231c89a613c96fd5a59b8d380da1
PagaMin <financeiroarthur09@gmail.com>
2025-02-22 08:56:15 -0300

    repository create

---
+++ src/main/java/com/example/auth_learning/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.example.auth_learning.repository;
+
+import com.example.auth_learning.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long>{
+}
